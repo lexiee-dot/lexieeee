55000163f38d22db741b2d9bde22c861b57b7bae
lexiee-dot <zaituo_0115@qq.com>
2026-06-23 21:42:29 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url);
+  if (req.url === '/') filePath = path.join(__dirname, 'index.html');
+  
+  const ext = path.extname(filePath);
+  const contentTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg': 'image/svg+xml'
+  };
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentTypes[ext] || 'text/plain' });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
